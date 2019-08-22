4fb955c46d36522f37d6739e7e6eac5fa11fd013
Eduardo Catunda e Silva <51955269+EduCatunda@users.noreply.github.com>
2019-08-22 01:47:06 -0300

    Java Code
    The main Java code

---
+++ CadCli.java
@@ -0,0 +1,17 @@
+package com.example.comprascrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadCli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_cli);
+
+
+
+    }
+}
